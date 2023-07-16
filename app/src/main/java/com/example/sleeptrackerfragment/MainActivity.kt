8fc5927f13653165e44e059d1032cd569142db22
jeanluckawel <jeanluckawel45@yahoo.com>
2023-07-16 10:35:36 -0800

    Header

--- app/src/main/java/com/example/sleeptrackerfragment/MainActivity.kt
+++ app/src/main/java/com/example/sleeptrackerfragment/MainActivity.kt
@@ -3,14 +3,31 @@ package com.example.sleeptrackerfragment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sleeptrackerfragment.ui.theme.SleepTrackerFragmentTheme
+import androidx.compose.material3.Text as Text1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +39,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
+@Composable
+fun ButtonCoonpoment(button_name:String){
+    Button(onClick = { /*TODO*/ }) {
+        Text(
+            text = button_name,
+            fontSize = 20.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopBar(title: String) {
+    Column{
+        Text(
+            text = title,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Row{
+                ButtonCoonpoment(button_name = "Start")
+                Spacer(modifier = Modifier.height(16.dp))
+                ButtonCoonpoment(button_name = "End")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SleepTrackerFragmentTheme {
-        Greeting("Android")
+      TopBar(title = "Track My Sleep Quality")
     }
 }
