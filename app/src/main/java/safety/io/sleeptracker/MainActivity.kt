39cb581df6fef61325f9717ecbbbc8803d7e06a6
Paul kafanda <paulkaf84@gmail.com>
2023-07-17 10:03:31 +0200

    fix header

--- app/src/main/java/safety/io/sleeptracker/MainActivity.kt
+++ app/src/main/java/safety/io/sleeptracker/MainActivity.kt
@@ -3,13 +3,28 @@ package safety.io.sleeptracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import safety.io.sleeptracker.ui.theme.SleepTrackerFragmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+
                 }
             }
         }
@@ -30,17 +45,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Header(text: String, backIcon: ImageVector?, background: Color = Color.DarkGray) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(background)
+            .height(32.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.width(12.dp))
+        if (null != backIcon) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = backIcon, contentDescription = null, tint = Color.White)
+            }
+        }
+        Text(text = text, color = Color.White)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
+fun HeaderPreview() {
     SleepTrackerFragmentTheme {
-        Greeting2("Android")
+        Header(
+            text = "Bus Schedule",
+            backIcon = Icons.Default.ArrowBack,
+            background = Color(0xFF9C27B0)
+        )
     }
 }
