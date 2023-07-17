7d8041ba5b07974092df5b8d613b1a1267dc2903
jeanluckawel <jeanluckawel45@yahoo.com>
2023-07-16 22:23:25 -0800

    body

--- app/src/main/java/safety/io/sleeptracker/MainActivity.kt
+++ app/src/main/java/safety/io/sleeptracker/MainActivity.kt
@@ -3,14 +3,25 @@ package safety.io.sleeptracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import safety.io.sleeptracker.ui.theme.SleepTrackerFragmentTheme
+import java.time.format.TextStyle
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +33,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
                 }
             }
         }
@@ -30,17 +40,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    SleepTrackerFragmentTheme {
-        Greeting2("Android")
+fun Body(stop:String, arrival:String, name:String, time:Date){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+           Text(text = stop)
+          Text(text = arrival)
+        }
+        Spacer(modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = name)
+            Text(text = time.toString())
+        }
     }
 }
