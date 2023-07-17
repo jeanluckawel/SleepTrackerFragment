c4f4b2407c13623becb11eb8bda34e7f27be6c1e
Paul kafanda <paulkaf84@gmail.com>
2023-07-17 10:49:20 +0200

    fix import

--- app/src/main/java/safety/io/sleeptracker/MainActivity.kt
+++ app/src/main/java/safety/io/sleeptracker/MainActivity.kt
@@ -26,6 +26,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import safety.io.sleeptracker.ui.theme.SleepTrackerFragmentTheme
