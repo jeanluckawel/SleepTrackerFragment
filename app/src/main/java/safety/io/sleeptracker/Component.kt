df3d331e17139757cd761707bf5b330ff90c7a83
Paul kafanda <paulkaf84@gmail.com>
2023-07-17 23:46:05 +0200

    fix modularity

---
+++ app/src/main/java/safety/io/sleeptracker/Component.kt
@@ -0,0 +1,120 @@
+package safety.io.sleeptracker
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import safety.io.sleeptracker.ui.theme.SleepTrackerFragmentTheme
+import java.util.Date
+
+@Composable
+fun ListLabel(stop:String, arrival:String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(start = 12.dp, end = 12.dp, top = 20.dp, bottom = 6.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = stop, color = Color.DarkGray)
+            Text(text = arrival, color = Color.DarkGray)
+        }
+        Divider(color = Color.DarkGray, thickness = 0.5.dp, modifier = Modifier.padding(bottom = 8.dp))
+    }
+}
+
+@Composable
+fun ListItem(name:String, time: Date) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                Color(0xFFDBC6E0)/*,
+                shape = RoundedCornerShape(12.dp)*/
+            ),
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = name, color = Color.DarkGray)
+            Text(text = time.toString(), color = Color.DarkGray)
+        }
+    }
+}
+
+@Composable
+fun Header(text: String, backIcon: ImageVector?, background: Color = Color.DarkGray) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(background)
+            .height(52.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.width(12.dp))
+        if (null != backIcon) {
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+            ) {
+                Icon(imageVector = backIcon, contentDescription = null, tint = Color.White)
+            }
+        }
+        Text(text = text, color = Color.White)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListItemPreview() {
+    SleepTrackerFragmentTheme {
+        ListItem(name = "Name 1", time = Date())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    SleepTrackerFragmentTheme {
+        Header(
+            text = "Bus Schedule",
+            backIcon = Icons.Default.ArrowBack,
+            background = Color(0xFF9C27B0)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BodyPreview() {
+    SleepTrackerFragmentTheme {
+        ListLabel(stop = "Stop 1", arrival = "Arrival 1")
+    }
+}
--- app/src/main/java/safety/io/sleeptracker/MainActivity.kt
+++ app/src/main/java/safety/io/sleeptracker/MainActivity.kt
@@ -3,31 +3,18 @@ package safety.io.sleeptracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import safety.io.sleeptracker.ui.theme.SleepTrackerFragmentTheme
@@ -44,102 +31,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Main()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Body(stop:String, arrival:String, name:String, time:Date) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = stop,
-                fontFamily = FontFamily.SansSerif,
-                fontWeight = FontWeight.Medium
-            )
-            Text(
-                text = arrival,
-                fontFamily = FontFamily.SansSerif,
-                fontWeight = FontWeight.Medium
-            )
-        }
-        Spacer(modifier = Modifier.padding(8.dp))
-        Divider(
-            color = Color.Black,
-            thickness = 1.dp
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = name,
-                fontFamily = FontFamily.SansSerif,
-                fontWeight = FontWeight.Medium
-            )
-            Text(
-                text = time.toString(),
-                fontFamily = FontFamily.SansSerif,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    }
-}
-
-@Composable
-fun Header(text: String, backIcon: ImageVector?, background: Color = Color.DarkGray) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(background)
-            .height(52.dp),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Spacer(modifier = Modifier.width(12.dp))
-        if (null != backIcon) {
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-            ) {
-                Icon(imageVector = backIcon, contentDescription = null, tint = Color.White)
-            }
-        }
-        Text(text = text, color = Color.White)
-    }
-}
 
 @Composable
 fun Main() {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Header(text = "Bus Schedule", backIcon = null, background = Color(0xFF9C27B0))
-        Body(stop = "Stop 1", arrival = "Arrival 1", name = "Name 1", time = Date())
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HeaderPreview() {
-    SleepTrackerFragmentTheme {
-        Header(
-            text = "Bus Schedule",
-            backIcon = Icons.Default.ArrowBack,
-            background = Color(0xFF9C27B0)
-        )
+        ListLabel(stop = "Stop 1", arrival = "Arrival 1")
+        repeat(10) {
+            ListItem(name = "Name $it", time = Date())
+            Divider(modifier = Modifier.fillMaxSize(), color = Color.Transparent, thickness = 4.dp)
+        }
     }
 }
 
